6364803b31c003ea4e2dddc8189056137ee394ca
ptricaud@student.42.fr <ptricaud@f2r6s18.clusters.42paris.fr>
2026-02-26 18:52:00 +0100

    1ere couche d extract faite

--- src/main/main.c
+++ src/main/main.c
@@ -6,12 +6,49 @@
 /*   By: ptricaud <ptricaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 14:06:49 by ptricaud          #+#    #+#             */
-/*   Updated: 2026/02/23 18:37:00 by ptricaud         ###   ########.fr       */
+/*   Updated: 2026/02/26 18:50:11 by ptricaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3d.h"
 
+void tester(t_file cubfile)
+{
+    int i = 0;
+    printf("MAP_CONTENT : \n");
+    while(cubfile.map[i])
+    {
+        printf("%s", cubfile.map[i]);
+        i++;
+    }
+    printf("\n\n\n --------------------------------------------- \n\n\n");
+    printf("CEILFLOOR & RGB CONTENT : \n");
+    i = 0;
+    while(cubfile.ceilfloor[i])
+    {
+        printf("%s\n",cubfile.ceilfloor[i]);
+        i++;
+    }
+    printf("\n\n\n --------------------------------------------- \n\n\n");
+    i = 0;
+    while(cubfile.RGB_c[i] && cubfile.RGB_f[i])
+    {
+        printf("value [%d] : RGB_c -->%d && RGB_f-->%d\n", i, cubfile.RGB_c[i], cubfile.RGB_f[i]);
+        i++;
+    }
+    printf("\n\n\n --------------------------------------------- \n\n\n");
+    while(cubfile.RGB_c[i])
+    printf("\n\n\n --------------------------------------------- \n\n\n");
+    printf("TEXTURE CONTENT : \n");
+    printf("wt_line[0] is :%s\n\n", cubfile.wt_line[0]);
+    printf("wt_line[1] is :%s\n\n", cubfile.wt_line[1]);
+    printf("wt_line[2] is :%s\n\n", cubfile.wt_line[2]);
+    printf("wt_line[3] is :%s\n\n", cubfile.wt_line[3]);
+    printf("texture_file[0] is :%s\n\n", cubfile.texture_file[0]);
+    printf("texture_file[1] is :%s\n\n", cubfile.texture_file[1]);
+    printf("texture_file[2] is :%s\n\n", cubfile.texture_file[2]);
+    printf("texture_file[3] is :%s\n\n", cubfile.texture_file[3]);
+}
 int	main(int ac, char **av)
 {
     char **file_tab;
@@ -29,6 +66,7 @@ int	main(int ac, char **av)
     file_tab = NULL;
     file_tab = alloc_file_size(file_tab, cubfile.fd, fd2);
     cubfile = split_file(cubfile, file_tab);
+    tester(cubfile);
     gc_mem(FULL_CLEAN, 0, NULL, GEN);
     return (0);
 }
